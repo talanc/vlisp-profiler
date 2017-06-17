f1e9ddfe82d8cf961b23e1127695b0b3b6ce6758
Thomas Copeland <talan.copeland@gmail.com>
2017-06-17 15:04:52 +1000

    added commandline

--- VLispProfiler/Program.cs
+++ VLispProfiler/Program.cs
@@ -11,63 +11,33 @@ namespace VLispProfiler
     {
         static int Main(string[] args)
         {
-            var result = CommandLine.Parser.Default.ParseArguments<ProfileVerb, ViewVerb>(args).MapResult(
-                (ProfileVerb opts) => RunProfile(opts),
-                (ViewVerb opts) => RunView(opts),
-                errs => 1);
+            int result = 0;
+
+#if RELEASE
+            try
+            {
+#endif
+                result = CommandLine.Parser.Default.ParseArguments<ProfileVerb, ViewVerb>(args).MapResult(
+                    (ProfileVerb opts) => RunProfile(opts),
+                    (ViewVerb opts) => RunView(opts),
+                    errs => 1);
+#if RELEASE
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return 1;
+            }
+#endif
 
             return result;
-
-            //var options = new Options();
-            //var isValid = CommandLine.Parser.Default.ParseArguments<Options>(args);
-
-            //if (isValid.Tag == ParserResultType.NotParsed)
-            //    Environment.Exit(1);
-
-            //string lispPath;
-
-            //if (args[0] == "--prompt")
-            //{
-            //    Console.Write("lisp file? ");
-            //    lispPath = Console.ReadLine();
-            //}
-            //else
-            //{
-            //    lispPath = args[0];
-            //}
-
-            ////if (!File.Exists(lispPath))
-            //    //ExitUsage("could not find file");
-
-            //var lispText = File.ReadAllText(lispPath);
-            //var profiler = new ProfilerEmitter(lispText);
-            //var emit = profiler.Emit();
-
-            //var symbolPath = $"{lispPath}.symbols.txt";
-            //File.WriteAllText(symbolPath, emit.Symbol);
-
-            //var profilerPath = $"{lispPath}.profiler.lsp";
-            //File.WriteAllText(profilerPath, emit.Profile);
-        }
-
-        static int RunProfile(ProfileVerb verb)
-        {
-            return 1;
-        }
-
-        static int RunView(ViewVerb verb)
-        {
-            return 1;
         }
 
         [Verb("profile", HelpText = "Generate Profile LISP file and Symbols file.")]
         class ProfileVerb
         {
-            [Option('f', "file", SetName = "File", HelpText = "LISP File")]
-            public string LispFile { get; set; }
-
-            [Option(SetName = "Prompt")]
-            public bool Prompt { get; set; }
+            [Option('f', "file", SetName = "File", Required = true, HelpText = "LISP Files")]
+            public IEnumerable<string> LispFiles { get; set; }
 
             [Option('i', "include", HelpText = "Includes (i.e. specify 'command' to only profile command calls)")]
             public IEnumerable<string> Includes { get; set; }
@@ -76,12 +46,77 @@ namespace VLispProfiler
             public IEnumerable<string> PredefinedSymbols { get; set; }
         }
 
+        static int RunProfile(ProfileVerb verb)
+        {
+            var err = 0;
+            
+            foreach (var file in verb.LispFiles)
+            {
+                if (!File.Exists(file))
+                {
+                    Console.WriteLine($"err: could not find file '{file}'");
+                    err = 1;
+                }
+            }
+            if (err != 0)
+                return err;
+
+            var symbols = new List<(int, string)>();
+            foreach (var sym in verb.PredefinedSymbols)
+            {
+                var i = sym.IndexOf(':');
+                if (i == -1)
+                {
+                    Console.WriteLine($"err: invalid predefined symbol '{sym}'");
+                    err = 1;
+                }
+                else
+                {
+                    var id = int.Parse(sym.Substring(0, i));
+                    var symbol = sym.Substring(i + 1);
+                    symbols.Add((id, symbol));
+                }
+            }
+            if (err != 0)
+                return err;
+
+            foreach (var filePath in verb.LispFiles)
+            {
+                var fileText = File.ReadAllText(filePath);
+
+                var profiler = new ProfilerEmitter(fileText);
+
+                foreach (var inc in verb.Includes)
+                    profiler.IncludeFilter.Add(inc);
+
+                foreach (var sym in symbols)
+                    profiler.AddPredefinedSymbol(sym.Item1, sym.Item2);
+
+                var emit = profiler.Emit();
+
+                var symbolPath = $"{filePath}.symbols.txt";
+                File.WriteAllText(symbolPath, emit.Symbol);
+
+                var profilerPath = $"{filePath}.prof.lsp";
+                File.WriteAllText(profilerPath, emit.Profile);
+            }
+
+            return 0;
+        }
+
         [Verb("view", HelpText = "View Profiler Results.")]
         class ViewVerb
         {
             [Value(0)]
-            [Option('f', "file", Required = true, HelpText = "LISP File")]
-            public string LispFile { get; set; }
+            [Option('f', "file", Required = true, HelpText = "LISP Files")]
+            public IEnumerable<string> LispFiles { get; set; }
+        }
+
+        static int RunView(ViewVerb verb)
+        {
+            Console.WriteLine("not implemented yet");
+
+            return 1;
         }
     }
 }
