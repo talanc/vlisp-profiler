7176361b07b2e9516d9b851058e37c27d54808c6
Thomas Copeland <talan.copeland@gmail.com>
2019-05-31 17:25:40 +1000

    removed duplicate (and misplaced) test

--- VLispProfiler.Tests/TraceReaderTests.cs
+++ VLispProfiler.Tests/TraceReaderTests.cs
@@ -57,47 +57,5 @@ namespace VLispProfiler.Tests
             Assert.AreEqual(2, root.Items.ElementAt(0).Items.ElementAt(0).Id);
             Assert.AreEqual(3, root.Items.ElementAt(0).Items.ElementAt(1).Id);
         }
-
-        [DataTestMethod]
-        [DataRow("list defun  oops")]
-        [DataRow("3 3.14 \"3.14\"")]
-        [DataRow("'list ''oops")]
-        [DataRow("(list 3 3.14 \"3.14\")")]
-        [DataRow("(defun a () 1)")]
-        [DataRow("(lambda () 1)")]
-        public void TestEmitter(string input)
-        {
-            // Arrange
-            var emitter = MakeEmitter(input);
-
-            // Act
-            var emit = emitter.Emit();
-
-            // Assert
-            Assert.AreEqual(Format(input), emit);
-        }
-
-        #region "Helpers"
-
-        private Emitter MakeEmitter(string sourceText)
-        {
-            var scanner = new Scanner(sourceText);
-            var parser = new Parser(scanner);
-            var program = parser.GetProgram();
-            var emitter = new Emitter(program, scanner);
-            return emitter;
-        }
-
-        private string Format(string sourceText)
-        {
-            var scanner = new Scanner(sourceText);
-            var parser = new Parser(scanner);
-            var program = parser.GetProgram();
-            var emitter = new Emitter(program, scanner);
-            return emitter.Emit();
-        }
-
-        #endregion
     }
-
 }
