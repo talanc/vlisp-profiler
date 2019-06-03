a3a7381ef2c3a5f5f88e46f7d3780bbd85ef8f22
Thomas Copeland <talan.copeland@gmail.com>
2019-06-03 13:27:43 +1000

    showing elapsed in top command (fixes #5)

--- VLispProfiler/View/Top.cs
+++ VLispProfiler/View/Top.cs
@@ -9,20 +9,60 @@ namespace VLispProfiler.View
 {
     public class Top
     {
-        private TextReader _input;
+        private VLispPath _path;
         private TextWriter _output;
         private int _top;
+        private TraceSummary _summary;
+        private SymbolFile _symbolFile;
 
         public Top(string filePath, TextWriter output, int top)
         {
-            _input = new StreamReader(filePath);
+            _path = new VLispPath(filePath);
             _output = output;
             _top = top;
+
+            using (var sr = new StreamReader(_path.TracesPath))
+            {
+                var reader = new TraceReader(sr);
+                var traceItem = reader.GetTrace();
+                _summary = new TraceSummary(traceItem);
+            }
+
+            _symbolFile = SymbolReader.ReadSymbolFile(_path.SymbolPath);
         }
 
         public void Display()
         {
-            _output.WriteLine($"TODO do top {_top}");
+            var elapsed = _summary.GetTopElapsed().Take(_top);
+            _output.WriteLine("Rank,Elapsed,Elapsed (Self),Called,Pos,Preview");
+            var i = 0;
+            foreach (var item in elapsed)
+            {
+                i++;
+                var symbol = _symbolFile.GetOrDefault(item.Id);
+
+                var preview = "?";
+                var pos = "?";
+                if (item.Id == 0)
+                {
+                    preview = "Root";
+                    pos = "N/A";
+                }
+                else if (symbol != null)
+                {
+                    preview = string.IsNullOrWhiteSpace(symbol.Preview) ? symbol.SymbolType : symbol.Preview;
+                    pos = symbol.StartPos.ToString();
+                }
+                else
+                {
+                    preview = $"ID={item.Id}";
+                    pos = "?";
+                }
+
+                _output.WriteLine($"{i},{TimeStr(item.Elapsed)},{TimeStr(item.SelfElapsed)},{item.Called},{pos},{preview}");
+            }
         }
+
+        private string TimeStr(TimeSpan value) => value.TotalMilliseconds.ToString("0") + "ms";
     }
 }
